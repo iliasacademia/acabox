6ae6069ab7b6891c2d0299b4eec8022d6bed6c27
Sundar Visweswaran <sundar@academia.edu>
2025-12-05 11:43:51 -0800

    Start at login in production, scroll behavior change

---
+++ src/renderer/components/DevelopmentBanner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DevelopmentBanner.css';
+
+const DevelopmentBanner: React.FC = () => {
+  return (
+    <div className="development-banner">
+      <span className="development-banner-text">
+        Development Mode
+      </span>
+    </div>
+  );
+};
+
+export default DevelopmentBanner;
